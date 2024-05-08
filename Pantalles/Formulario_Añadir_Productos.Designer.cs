ea06a51d2291714f4a26b8b83bd8062f4c9ecabe
Pedro Sala <pedrosalamora@gmail.com>
2024-05-08 09:45:59 +0200

    commit 8-5-2024

---
+++ Pantalles/Formulario_Añadir_Productos.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Projecte_programació.Pantalles
+{
+    partial class Formulario_Añadir_Productos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBnombre = new System.Windows.Forms.TextBox();
+            this.TBtemporada = new System.Windows.Forms.TextBox();
+            this.TBprecio = new System.Windows.Forms.TextBox();
+            this.NUDcantidad = new System.Windows.Forms.NumericUpDown();
+            this.RBfruta = new System.Windows.Forms.RadioButton();
+            this.RBverdura = new System.Windows.Forms.RadioButton();
+            this.RBhortaliza = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDcantidad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(577, 66);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(586, 91);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "WOLLA-DONA";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(641, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 29);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(600, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 29);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Temporada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(619, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 29);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Precio/Kg";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(631, 460);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 29);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Cantidad";
+            // 
+            // TBnombre
+            // 
+            this.TBnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBnombre.Location = new System.Drawing.Point(810, 227);
+            this.TBnombre.Name = "TBnombre";
+            this.TBnombre.Size = new System.Drawing.Size(275, 34);
+            this.TBnombre.TabIndex = 18;
+            // 
+            // TBtemporada
+            // 
+            this.TBtemporada.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBtemporada.Location = new System.Drawing.Point(810, 299);
+            this.TBtemporada.Name = "TBtemporada";
+            this.TBtemporada.Size = new System.Drawing.Size(275, 34);
+            this.TBtemporada.TabIndex = 19;
+            // 
+            // TBprecio
+            // 
+            this.TBprecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBprecio.Location = new System.Drawing.Point(810, 377);
+            this.TBprecio.Name = "TBprecio";
+            this.TBprecio.Size = new System.Drawing.Size(275, 34);
+            this.TBprecio.TabIndex = 20;
+            // 
+            // NUDcantidad
+            // 
+            this.NUDcantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NUDcantidad.Location = new System.Drawing.Point(810, 455);
+            this.NUDcantidad.Name = "NUDcantidad";
+            this.NUDcantidad.Size = new System.Drawing.Size(275, 34);
+            this.NUDcantidad.TabIndex = 21;
+            // 
+            // RBfruta
+            // 
+            this.RBfruta.AutoSize = true;
+            this.RBfruta.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBfruta.ForeColor = System.Drawing.Color.White;
+            this.RBfruta.Location = new System.Drawing.Point(654, 556);
+            this.RBfruta.Name = "RBfruta";
+            this.RBfruta.Size = new System.Drawing.Size(94, 33);
+            this.RBfruta.TabIndex = 22;
+            this.RBfruta.TabStop = true;
+            this.RBfruta.Text = "Fruta";
+            this.RBfruta.UseVisualStyleBackColor = true;
+            // 
+            // RBverdura
+            // 
+            this.RBverdura.AutoSize = true;
+            this.RBverdura.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBverdura.ForeColor = System.Drawing.Color.White;
+            this.RBverdura.Location = new System.Drawing.Point(847, 556);
+            this.RBverdura.Name = "RBverdura";
+            this.RBverdura.Size = new System.Drawing.Size(126, 33);
+            this.RBverdura.TabIndex = 23;
+            this.RBverdura.TabStop = true;
+            this.RBverdura.Text = "Verdura";
+            this.RBverdura.UseVisualStyleBackColor = true;
+            // 
+            // RBhortaliza
+            // 
+            this.RBhortaliza.AutoSize = true;
+            this.RBhortaliza.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBhortaliza.ForeColor = System.Drawing.Color.White;
+            this.RBhortaliza.Location = new System.Drawing.Point(1051, 556);
+            this.RBhortaliza.Name = "RBhortaliza";
+            this.RBhortaliza.Size = new System.Drawing.Size(137, 33);
+            this.RBhortaliza.TabIndex = 24;
+            this.RBhortaliza.TabStop = true;
+            this.RBhortaliza.Text = "Hortaliza";
+            this.RBhortaliza.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(126)))), ((int)(((byte)(167)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(832, 704);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 89);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Formulario_Añadir_Productos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(86)))), ((int)(((byte)(95)))));
+            this.ClientSize = new System.Drawing.Size(1845, 998);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RBhortaliza);
+            this.Controls.Add(this.RBverdura);
+            this.Controls.Add(this.RBfruta);
+            this.Controls.Add(this.NUDcantidad);
+            this.Controls.Add(this.TBprecio);
+            this.Controls.Add(this.TBtemporada);
+            this.Controls.Add(this.TBnombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Name = "Formulario_Añadir_Productos";
+            this.Text = "Formulario_Añadir_Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.NUDcantidad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TBnombre;
+        private System.Windows.Forms.TextBox TBtemporada;
+        private System.Windows.Forms.TextBox TBprecio;
+        private System.Windows.Forms.NumericUpDown NUDcantidad;
+        private System.Windows.Forms.RadioButton RBfruta;
+        private System.Windows.Forms.RadioButton RBverdura;
+        private System.Windows.Forms.RadioButton RBhortaliza;
+        private System.Windows.Forms.Button button1;
+    }
+}
